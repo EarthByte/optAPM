84879108358f5dd10eedb87e9edd324cc102bd9e
John Cannon <john.cannon@sydney.edu.au>
2021-05-07 23:10:25 +1000

    Only calculate fragmentation index for continents, not topological plates.
    And only calculate fragmentation index if requested.

--- continent_fragmentation.py
+++ continent_fragmentation.py
@@ -19,40 +19,35 @@ class ContinentFragmentation(object):
             data_dir,
             original_rotation_filenames,  # Relative to the 'data/' directory.
             continent_features,
-            continent_features_are_topologies,
-            # Point spacing used when creating continent contours (only used if continent_features_are_topologies is False)...
+            # Point spacing used when creating continent contours...
             continent_contouring_point_spacing_degrees,
-            # Area threshold (in square radians) when creating continent contours (only used if continent_features_are_topologies is False)...
+            # Area threshold (in square radians) when creating continent contours...
             continent_contouring_area_threshold_steradians,
             age_range):
         """
         Load the continent features and use *original* rotation model to calculate fragment through all time to find normalisation factor.
-        
-        The normalisation factor should be such that approx 1.0 will represent the maximum fragmentation when later using optimised
-        rotation model to calculate fragmentation at each time interval.
         """
         
         self.data_dir = data_dir
         self.continent_features = continent_features
-        self.continent_features_are_topologies = continent_features_are_topologies
 
-        # When continents are regular static polygons (not dynamic topologies) we need a point grid to calculate
-        # contour polygons representing the boundary of reconstructed static polygons that overlap each other.
-        if not self.continent_features_are_topologies:
-            self.contouring_area_threshold_steradians = continent_contouring_area_threshold_steradians
-            self.contouring_point_spacing_degrees = continent_contouring_point_spacing_degrees
-            lons = np.arange(-180.0, 180.001, self.contouring_point_spacing_degrees)
-            lats = np.arange(-90.0, 90.001, self.contouring_point_spacing_degrees)
-            self.contouring_grid_dimensions = len(lats), len(lons)
+        # A point grid to calculate contour polygons representing the boundary of reconstructed static polygons that overlap each other.
+        self.contouring_area_threshold_steradians = continent_contouring_area_threshold_steradians
+        self.contouring_point_spacing_degrees = continent_contouring_point_spacing_degrees
+        lons = np.arange(-180.0, 180.001, self.contouring_point_spacing_degrees)
+        lats = np.arange(-90.0, 90.001, self.contouring_point_spacing_degrees)
+        self.contouring_grid_dimensions = len(lats), len(lons)
 
-            contouring_longitude_array, contouring_latitude_array = np.meshgrid(lons, lats)
-            self.contouring_points = pygplates.MultiPointOnSphere(
-                    zip(contouring_latitude_array.flatten(), contouring_longitude_array.flatten()))
-            
-            self.debug_contour_polygons = False
-            if self.debug_contour_polygons:
-                self.debug_contour_polygon_features = []
-                self.debug_time_interval = age_range[1] - age_range[0]
+        self.age_range = age_range
+
+        contouring_longitude_array, contouring_latitude_array = np.meshgrid(lons, lats)
+        self.contouring_points = pygplates.MultiPointOnSphere(
+                zip(contouring_latitude_array.flatten(), contouring_longitude_array.flatten()))
+        
+        self.debug_contour_polygons = False
+        if self.debug_contour_polygons:
+            self.debug_contour_polygon_features = []
+            self.debug_time_interval = age_range[1] - age_range[0]
         
         # Load all the original rotation feature collections.
         rotation_features = []
@@ -65,15 +60,13 @@ class ContinentFragmentation(object):
         # Load all original rotations into a rotation model.
         self.rotation_model = pygplates.RotationModel(rotation_features)
         
-        # Find the maximum fragmentation over the age range.
-        print('Calculating continental fragmentation for {0}-{1}Ma...'.format(age_range[0], age_range[-1]))
-        sys.stdout.flush()
-        self.fragmentations = dict((age, self._calculate_fragmentation(age)) for age in age_range)
-        self.max_fragmentation = max(self.fragmentations.values())
-        #print('  min:', np.min(list(self.fragmentations.values())) / self.max_fragmentation)
-        #print('  mean:', np.mean(list(self.fragmentations.values())) / self.max_fragmentation)
-        #print('  dev:', np.std(list(self.fragmentations.values())) / self.max_fragmentation)
-        #sys.stdout.flush()
+        # A dict mapping age to global fragmentation index.
+        # This gets populated as needed.
+        self.fragmentations = {}
+
+        # We'll calculate this later if a *normalized* fragmentation index is ever requested since
+        # that'll require calculating fragmentation indices over the entire age range.
+        self.max_fragmentation = None
 
         # Debug output contour polygons to GPML.
         if self.debug_contour_polygons:
@@ -85,12 +78,33 @@ class ContinentFragmentation(object):
             age,
             normalize=True):
         """
-        Calculate the normalised continental fragmentation index at the specified time.
+        Calculate the continental fragmentation index at the specified time.
+        
+        If *normalize* is True then the normalisation factor will be such that approx 1.0 will represent the
+        maximum fragmentation over the age range specified in constructor.
         """
 
+        # Calculate and cache result in dict (if not already cached).
+        if age not in self.fragmentations:
+            self.fragmentations[age] = self._calculate_fragmentation(age)
+        
         fragmentation = self.fragmentations[age]
 
         if normalize:
+            # First calculate the maximum fragmentation index (if we haven't already).
+            if not self.max_fragmentation:
+                print('Calculating continental fragmentation for {0}-{1}Ma...'.format(self.age_range[0], self.age_range[-1]))
+                sys.stdout.flush()
+
+                # Find the maximum fragmentation over the age range.
+                # This also caches the fragmentation values over the age range.
+                self.max_fragmentation = max(self.get_fragmentation(age, normalize=False) for age in self.age_range)
+
+                #print('  min:', np.min(list(self.fragmentations.values())) / self.max_fragmentation)
+                #print('  mean:', np.mean(list(self.fragmentations.values())) / self.max_fragmentation)
+                #print('  dev:', np.std(list(self.fragmentations.values())) / self.max_fragmentation)
+                #sys.stdout.flush()
+            
             # A *normalised* version of the pre-calculated fragmentation at age.
             fragmentation /= self.max_fragmentation
 
@@ -104,80 +118,81 @@ class ContinentFragmentation(object):
         total_perimeter = 0.0
         total_area = 0.0
         
-        # Resolve topological plate polygons or reconstruct static continental polygons.
-        if self.continent_features_are_topologies:
-            # Resolve the topological plate polygons for the current time.
-            resolved_topologies = []
-            pygplates.resolve_topologies(
-                    self.continent_features,
-                    self.rotation_model,
-                    resolved_topologies,
-                    age)
+        # Reconstruct static continental polygons.
+        reconstructed_feature_geometries = []
+        pygplates.reconstruct(self.continent_features, self.rotation_model, reconstructed_feature_geometries, age)
+        
+        # Get a list of polygons.
+        #
+        # We should have polygons (not polylines) but turn into a polygon if happens to be a polyline
+        # (but that actually only works if the polyline is a closed loop and not just part of a polygon's boundary).
+        reconstructed_polygons = [pygplates.PolygonOnSphere(reconstructed_feature_geometry.get_reconstructed_geometry())
+                for reconstructed_feature_geometry in reconstructed_feature_geometries]
+        
+        # Calculate contour polygons representing the boundary(s) of the reconstructed static polygons that overlap each other.
+        reconstructed_contour_polygons = self.get_contour_polygons(reconstructed_polygons)
 
-            # Iterate over the resolved topologies.
-            for resolved_topology in resolved_topologies:
-                total_perimeter += resolved_topology.get_resolved_boundary().get_arc_length()
-                total_area += resolved_topology.get_resolved_boundary().get_area()
-            
-        else:
-            # Reconstruct the static continental polygons.
-            reconstructed_feature_geometries = []
-            pygplates.reconstruct(self.continent_features, self.rotation_model, reconstructed_feature_geometries, age)
-            
-            # Get a list of polygons.
-            #
-            # We should have polygons (not polylines) but turn into a polygon if happens to be a polyline
-            # (but that actually only works if the polyline is a closed loop and not just part of a polygon's boundary).
-            reconstructed_polygons = [pygplates.PolygonOnSphere(reconstructed_feature_geometry.get_reconstructed_geometry())
-                    for reconstructed_feature_geometry in reconstructed_feature_geometries]
-            
-            # When continents are regular static polygons (not dynamic topologies) we need calculate contour polygons
-            # representing the boundary(s) of the reconstructed static polygons that overlap each other.
-            reconstructed_contour_polygons = self._calculate_contour_polygons(reconstructed_polygons)
-            
-            # Contour polygons smaller than this will be excluded.
-            min_area = self.contouring_area_threshold_steradians
-            # A contour polygon's area should not be more than half the global area.
-            # It seems this can happen with pygplates revisions prior to 31 when there's a sliver polygon along the dateline
-            # (that gets an area that's a multiple of PI, instead of zero).
-            max_area = 2 * math.pi - 1e-4
+        # Update total perimeter and area.
+        for reconstructed_contour_polygon in reconstructed_contour_polygons:
+            total_perimeter += reconstructed_contour_polygon.get_arc_length()
+            total_area += reconstructed_contour_polygon.get_area()
 
-            for reconstructed_contour_polygon in reconstructed_contour_polygons:
-                reconstructed_contour_polygon_area = reconstructed_contour_polygon.get_area()
-                # Exclude contour polygon if smaller than the threshold.
-                if (reconstructed_contour_polygon_area > min_area and
-                    reconstructed_contour_polygon_area < max_area):
-                    total_perimeter += reconstructed_contour_polygon.get_arc_length()
-                    total_area += reconstructed_contour_polygon_area
-
-                    # Debug output contour polygons.
-                    if self.debug_contour_polygons:
-                        self.debug_contour_polygon_features.append(
-                                pygplates.Feature.create_reconstructable_feature(
-                                        pygplates.FeatureType.gpml_unclassified_feature,
-                                        reconstructed_contour_polygon,
-                                        valid_time=(age + 0.5 * self.debug_time_interval, age - 0.5 * self.debug_time_interval)))
+        # Debug output contour polygons.
+        if self.debug_contour_polygons:
+            self.debug_contour_polygon_features.extend(
+                    pygplates.Feature.create_reconstructable_feature(
+                            pygplates.FeatureType.gpml_unclassified_feature,
+                            reconstructed_contour_polygon,
+                            valid_time=(age + 0.5 * self.debug_time_interval, age - 0.5 * self.debug_time_interval))
+                    for reconstructed_contour_polygon in reconstructed_contour_polygons)
 
         #print('age:', age, 'frag_index (1/km):', total_perimeter / total_area / 6371.0); sys.stdout.flush()
         return total_perimeter / total_area
     
     
+    def get_contour_polygons(
+            self,
+            continent_polygons):
+        """
+        Find the boundaries of the specified (potentially overlapping/abutting) continent polygons as contour polygons.
+        """
+
+        contour_polygons = self._calculate_contour_polygons(continent_polygons)
+        
+        # Contour polygons smaller than this will be excluded.
+        min_area = self.contouring_area_threshold_steradians
+        # A contour polygon's area should not be more than half the global area.
+        # It seems this can happen with pygplates revisions prior to 31 when there's a sliver polygon along the dateline
+        # (that gets an area that's a multiple of PI, instead of zero).
+        max_area = 2 * math.pi - 1e-4
+
+        contour_polygons_above_area_threshold = []
+        for contour_polygon in contour_polygons:
+            # Exclude contour polygon if smaller than the threshold.
+            contour_polygon_area = contour_polygon.get_area()
+            if (contour_polygon_area > min_area and
+                contour_polygon_area < max_area):
+                contour_polygons_above_area_threshold.append(contour_polygon)
+
+        return contour_polygons_above_area_threshold
+
+    
     def _calculate_contour_polygons(
             self,
-            polygons):
+            continent_polygons):
         """
-        Find the boundaries of the specified (potentially overlapping/abutting) polygons as contour polygons.
+        Find the boundaries of the specified (potentially overlapping/abutting) continent polygons as contour polygons.
 
         Note that code used in this function was copied from code written by Andrew Merdith and Simon Williams.
         See https://github.com/amer7632/pyGPlates_examples/blob/master/Merdith_2019_GPC/Perimeter-to-area-ratio.ipynb
         """
 
         # Find the reconstructed continental polygon (if any) containing each point.
-        polygons_containing_points = points_in_polygons.find_polygons(self.contouring_points, polygons)
+        continent_polygons_containing_points = points_in_polygons.find_polygons(self.contouring_points, continent_polygons)
 
         zval = []
-        for polygon_containing_point in polygons_containing_points:
-            if polygon_containing_point is not None:
+        for continent_polygon_containing_point in continent_polygons_containing_points:
+            if continent_polygon_containing_point is not None:
                 zval.append(1)
             else:
                 zval.append(0)
